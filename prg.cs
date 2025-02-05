5771b26d5df9c6a9a7aabd9297213699e65bb6b5
Hemant551147 <hamuugurjar@gmail.com>
2025-02-05 12:36:03 +0530

    Create prg.cs
    1.	Write a program to simply display all the imformation about yourself.

---
+++ prg.cs
@@ -0,0 +1,22 @@
+System;
+using
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string name = "Suman";
+        int rollNo = 123;
+        string className = "BCA";
+        string semester = "5th";
+        string subjectName = "C##";
+        string project = "Toll Tracker";
+        
+        Console.WriteLine ("Name: " + name);
+        Console.WriteLine("Roll No: " + rollNo);
+        Console.WriteLine("Class Name: " + className);
+        Console.WriteLine("Semester: " + semester);
+        Console.WriteLine("Subject Name: " + subjectName);
+        Console.WriteLine("Project: " + project);
+        
+    }
+}
